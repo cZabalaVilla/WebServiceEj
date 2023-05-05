aab2cfc3ebee951c5f1a19236b6240a4c620df9d
DIR\c.zabala.villanueva <c.zabala.villanueva@accenture.com>
2023-05-05 10:43:50 +0200

    commit

---
+++ fpdual/src/main/java/org/fpdual/web/fpdualweb/api/dto/Notification.java
@@ -0,0 +1,16 @@
+package org.fpdual.web.fpdualweb.api.dto;
+
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Builder
+public class Notification {
+    private int id;
+    private String title;
+    private String body;
+}
